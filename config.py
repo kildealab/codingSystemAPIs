3df63834c1117d5f72432506e2c8a97f2811d54a
Kayla O'Sullivan <k.osullivan.steben@hotmail.com>
2025-01-21 19:46:42 +0000

    Give instructions for obtaining api keys

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 ##########################################################
 # ICD API access variables
+# To get an access token, you must register at: https://icd.who.int/icdapi (see "API ACCESS")
 token_endpoint = 'https://icdaccessmanagement.who.int/connect/token'
 client_id = #'client id here'
 client_secret = #'client secret here'
@@ -13,9 +14,9 @@ payload = {'client_id': client_id,
 
 ##########################################################
 # UMLS API access variables
-umls_api_key =  'api key here'
+# To get an API key, you will need to register at: https://documentation.uts.nlm.nih.gov/rest/home.html (click "Get Your API Key")
+umls_api_key =  #'api key here'
 
 ##########################################################
 # SNOMED access variables 
 # N/A
-   
